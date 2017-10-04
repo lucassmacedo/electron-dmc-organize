652a8f44284eb6cfb765c31ee354bb05e4512545
Lucas Macedo <luuckymacedo@gmail.com>
2017-10-04 15:12:02 -0300

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+  	 <router-view name="menuView"></router-view>
+      <transition name="list">
+        <router-view></router-view>
+      </transition>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'dmc-organize'
+  }
+</script>
+
+<style>
+	/*@import "~bulma/css/bulma.css";*/
+	@import "~buefy/lib/buefy.css";
+</style>
---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,122 @@
+<template>
+  <div id="wrapper">
+    <div id="logo" v-show='showSlashScreen'>
+      <img src="~@/assets/logo.png" alt="electron-vue">
+    </div>
+  </div>
+</template>
+
+<script>
+  import SystemInformation from './LandingPage/SystemInformation'
+
+  export default {
+    name: 'landing-page',
+    components: { SystemInformation },
+    data () {
+      return {
+        showSlashScreen: true
+      }
+    },
+    methods: {
+      open (link) {
+        this.$electron.shell.openExternal(link)
+      }
+    },
+    mounted () {
+      console.log('1')
+      setTimeout(() => {
+        console.log('2')
+        this.showSlashScreen = false
+      }, 4000)
+    }
+  }
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body { font-family: 'Source Sans Pro', sans-serif; }
+
+  #wrapper {
+    background: #ccc;
+    height: 100vh;
+    padding: 60px 80px;
+    width: 100vw;
+  }
+
+  #logo {
+    background: #00aeef;
+    position:relative;
+    height: 100%;
+    width:100%;
+  } 
+
+  #logo img {
+    position:absolute;
+    top:0;
+    left:0;
+    right:0;
+    bottom:0;
+    margin:auto;
+  }
+
+  main {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  main > div { flex-basis: 50%; }
+
+  .left-side {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .welcome {
+    color: #555;
+    font-size: 23px;
+    margin-bottom: 10px;
+  }
+
+  .title {
+    color: #2c3e50;
+    font-size: 20px;
+    font-weight: bold;
+    margin-bottom: 6px;
+  }
+
+  .title.alt {
+    font-size: 18px;
+    margin-bottom: 10px;
+  }
+
+  .doc p {
+    color: black;
+    margin-bottom: 10px;
+  }
+
+  .doc button {
+    font-size: .8em;
+    cursor: pointer;
+    outline: none;
+    padding: 0.75em 2em;
+    border-radius: 2em;
+    display: inline-block;
+    color: #fff;
+    background-color: #4fc08d;
+    transition: all 0.15s ease;
+    box-sizing: border-box;
+    border: 1px solid #4fc08d;
+  }
+
+  .doc button.alt {
+    color: #42b983;
+    background-color: transparent;
+  }
+</style>
